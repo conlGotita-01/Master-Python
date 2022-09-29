f44c4082ba239641cc48f84dc9d83d61a49d4571
Brenda Torres Murcia <brenda13071@hotmail.com>
2022-09-29 13:53:25 -0500

    Variables in Python

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 
+# A variable will allow us the store information temporarity
 
 myVariable = "Hello from python"
 print(myVariable)
+print(myVariable)
+print(myVariable)
+print(myVariable)
 
+# The variable information can be changed as needed
+myVariable = 5
+
+print(myVariable)
