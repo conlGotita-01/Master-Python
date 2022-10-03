29293ca06b22007f723665acd3971834a6a5c97f
Brenda Torres Murcia <brenda13071@hotmail.com>
2022-10-03 18:04:54 -0500

    Python Data Types

--- pythonDataTypes.py
+++ pythonDataTypes.py
@@ -1,14 +1,25 @@
 
 x = 10
 y = 'A'
-z = True
+z = 16,5
+w = True
+
+# Integer type.
 
 print(x)
 print(type(x))
 
+# Char/String type.
+
 print(y)
 print(type(y))
+
+# Floating type.
 
 print(z)
 print(type(z))
 
+# Boolean type.
+
+print(w)
+print(type(x))
