0f6a7acf231cfcb170436fc92c107c61d7192fc3
Brenda Torres Murcia <brenda13071@hotmail.com>
2022-10-03 16:15:50 -0500

    Excercise Number One

---
+++ excercise1.py
@@ -0,0 +1,10 @@
+# Variable declaration
+
+name = 'Brenda Dayana Torres Murcia'
+number = 3202645126
+e_mail = 'brenditatomu_3@hotmail.com'
+
+# Display information 
+print(name)
+print(number)
+print(e_mail)
