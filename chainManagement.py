8f61559e9d6c02a5ba6637ffa9fdfe21e01bcca7
Brenda Torres Murcia <brenda13071@hotmail.com>
2022-10-04 16:02:30 -0500

    String Management

--- chainManagement.py
+++ chainManagement.py
@@ -2,6 +2,7 @@
 
 myFavoriteBand = "Zoe"
 
+
 print("My favorite rock band is " + myFavoriteBand)
 
 
@@ -9,5 +10,10 @@ num_1 = "1"
 num_2 = "2"
 
 # It is taught literally but not the arithmetic form of the sum.
-print(num_1 + num_2)
+
+print("Concatenation: ", num_1 + num_2)
+print("Sum:", int(num_1) + int(num_2))
+
+
+
 
