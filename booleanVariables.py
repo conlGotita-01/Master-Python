ddc61984ed64c0b64d9b927e181331c330ee1959
Brenda Torres Murcia <brenda13071@hotmail.com>
2022-10-05 16:36:07 -0500

    Boolean Variables

---
+++ booleanVariables.py
@@ -0,0 +1,14 @@
+
+
+myVariable = True
+
+print(myVariable)
+
+myVariable = 4 > 7
+
+print(myVariable)
+
+if myVariable:
+    print("El resultado fue verdadero")
+else: 
+    print("El resultado fue falso")    
