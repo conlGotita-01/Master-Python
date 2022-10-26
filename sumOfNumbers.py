e3c173b50e266458a4dc11e2c77096382ff2967d
Brenda Torres Murcia <brenda13071@hotmail.com>
2022-10-26 18:48:42 -0500

    Excercise data entry

---
+++ sumOfNumbers.py
@@ -0,0 +1,8 @@
+# Excercise data entry
+
+number_1 = int(input("Enter a number: "))
+number_2 = int(input("Enter another number: "))
+
+amount = number_1 + number_2
+
+print("The sum of the numbers is: ", amount)
