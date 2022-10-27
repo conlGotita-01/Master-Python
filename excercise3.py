fe41b4405fe9074fe72946d09ad9c574998898ca
Brenda Torres Murcia <brenda13071@hotmail.com>
2022-10-26 19:17:00 -0500

    Data Entry

---
+++ excercise3.py
@@ -0,0 +1,12 @@
+# Details of a book (Data Entry)
+
+
+
+
+title = str(input("Enter the book title: "))
+author = str(input("Enter the author name: "))
+
+print("Title: ", title)
+print("Author: ", author)
+print("The book name ", title, "was written by ", author)
+
